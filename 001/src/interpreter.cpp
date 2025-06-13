0bc6b19f97e11212f70a99183d07c536cbd41183
Rydex <112235340+Rydex@users.noreply.github.com>
2025-06-13 16:03:09 +0800

    extended interpreter functionality, just gonna leave this here for now

--- 001/src/interpreter.cpp
+++ 001/src/interpreter.cpp
@@ -1,13 +1,48 @@
 #include "interpreter.h"
 #include "parser.h"
+#include "position.h"
 #include <functional>
 #include <iostream>
 #include <stdexcept>
 
-void Interpreter::visit(const NodeVariant& node) {
-	std::function<void(const NodeVariant& node)> method = [this](const NodeVariant& node) {
+Number::Number(int value): value(value) {}
+
+Number& Number::set_pos(
+	const std::optional<Position>& pos_start,
+	const std::optional<Position>& pos_end
+) {
+	this->pos_start = pos_start;
+	this->pos_end = pos_end;
+
+	return *this;
+}
+
+Number Number::added_to(const Number& other) const {
+	return Number(this->value + other.value);
+}
+
+Number Number::subbed_by(const Number& other) const {
+	return Number(this->value - other.value);
+}
+
+Number Number::multiplied_by(const Number& other) const {
+	return Number(this->value * other.value);
+}
+
+Number Number::divided_by(const Number& other) const {
+	return Number(this->value / other.value);
+}
+
+std::string Number::as_string() const {
+	return std::to_string(value);
+}
+
+std::function<Number(const NodeVariant& node)> Interpreter::visit(const NodeVariant& node) {
+	std::function<Number(const NodeVariant& node)> method = [this](const NodeVariant& node) {
 		if(std::holds_alternative<NumberNode>(node)) {
-			visit_NumberNode(std::get<NumberNode>(node));
+			Number num = visit_NumberNode(std::get<NumberNode>(node));
+			return num;
+
 		} else if(std::holds_alternative<SharedBin>(node)) {
 			visit_BinOpNode(*std::get<SharedBin>(node));
 		} else if(std::holds_alternative<SharedUnary>(node)) {
@@ -17,20 +52,25 @@ void Interpreter::visit(const NodeVariant& node) {
 		}
 	};
 
-	method(node);
+	return method(node);
 }
 
-void Interpreter::visit_NumberNode(const NumberNode& node) {
-	std::cout << "found numbernode\n";
+Number Interpreter::visit_NumberNode(const NumberNode& node) {
+	Token node_value = node.tok.value();
+	TokenValue value = node_value.value.value();
+
+	if(std::holds_alternative<int>(value)) {
+		return Number(std::get<int>(value));
+	} else {
+		return Number(std::get<double>(value));
+	}
 }
 
-void Interpreter::visit_BinOpNode(const BinOpNode& node) {
-	std::cout << "found binopnode\n";
-	visit(node.left_node);
-	visit(node.right_node);
+Number Interpreter::visit_BinOpNode(const BinOpNode& node) {
+	NodeVariant left = visit();
 }
 
-void Interpreter::visit_UnaryOpNode(const UnaryOpNode& node) {
+Number Interpreter::visit_UnaryOpNode(const UnaryOpNode& node) {
 	std::cout << "found unaryopnode\n";
 	visit(node.node);
 }
--- 001/src/interpreter.h
+++ 001/src/interpreter.h
@@ -2,14 +2,36 @@
 #define INTERPRETER
 
 #include "parser.h"
+#include "position.h"
 #include <functional>
+
+class Number {
+protected:
+	int value;
+	std::optional<Position> pos_start, pos_end;
+
+public:
+	Number(int value);
+
+	Number& set_pos(
+		const std::optional<Position>& pos_start = std::nullopt,
+		const std::optional<Position>& pos_end = std::nullopt
+	);
+
+	Number added_to(const Number& other) const;
+	Number subbed_by(const Number& other) const;
+	Number multiplied_by(const Number& other) const;
+	Number divided_by (const Number& other) const;
+
+	std::string as_string() const;
+};
 
 class Interpreter {
 public:
 	void visit(const NodeVariant& node);
-	void visit_NumberNode(const NumberNode& node);
-	void visit_BinOpNode(const BinOpNode& node);
-	void visit_UnaryOpNode(const UnaryOpNode& node);
+	Number visit_NumberNode(const NumberNode& node);
+	Number visit_BinOpNode(const BinOpNode& node);
+	Number visit_UnaryOpNode(const UnaryOpNode& node);
 };
 
 #endif
